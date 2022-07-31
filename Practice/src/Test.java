49e9c37a7a0bd9d6fd13faeda59ff796dd34df16
zhang <x>
2022-08-01 03:57:29 +0800

    第一次提交

---
+++ Practice/src/Test.java
@@ -0,0 +1,20 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Test {
+
+
+    public static void main(String[] args) throws IOException {
+        String fileName = "E:\\hello.txt";
+        File file = new File(fileName);
+
+        InputStream in=new FileInputStream(file);
+        byte[] b=new byte[(int)file.length()];
+        in.read(b);
+        System.out.println("文件长度为："+file.length());
+        in.close();
+        System.out.println(new String(b));
+    }
+}
